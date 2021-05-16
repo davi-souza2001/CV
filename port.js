147ea64f96288009b165b26f9ef80d5b1da04f3c
davi-souza2001 <77704994+davi-souza2001@users.noreply.github.com>
2021-05-16 11:28:25 -0300

    Add files via upload

---
+++ port.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+
+});
+
+function open_link_ne(){
+    window.open('https://davi-souza2001.github.io/copy-of-Netflix/');
+};
+function open_link_gn(){
+    window.open('https://davi-souza2001.github.io/game-gnat/');
+};
+function open_link_hg(){
+    window.open('https://davi-souza2001.github.io/hash-game/');
+};
+function open_link_in(){
+    window.open('https://davi-souza2001.github.io/Copy-of-Instagram/');
+};
+function open_link_fp(){
+    window.open('https://davi-souza2001.github.io/form-purple/');
+};
+function open_link_fd(){
+    window.open('https://davi-souza2001.github.io/doom-game-fire/');
+};
+function open_link_mo(){
+    window.open('https://davi-souza2001.github.io/moon-project/')
+}
+function open_link_te(){
+    window.open('https://davi-souza2001.github.io/TecBlog/')
+}
+function open_link_mu(){
+    window.open('https://davi-souza2001.github.io/Museum-website/')
+}
